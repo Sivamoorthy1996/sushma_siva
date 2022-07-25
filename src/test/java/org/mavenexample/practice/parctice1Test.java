06cdda1cbf182eea89e3a2872dd767043f2898a1
Sivamoorthy1996 <sivamoorthynkl1996@gmail.com>
2022-07-25 12:53:07 +0530

    first

---
+++ src/test/java/org/mavenexample/practice/parctice1Test.java
@@ -0,0 +1,44 @@
+package org.mavenexample.practice;
+
+import org.testng.annotations.Test;
+
+public class parctice1Test {
+	
+	@Test
+	public void parctice1() {
+		String URL = System.getProperty("url");
+		String browser = System.getProperty("browser");
+		
+		System.out.println("url-->"+URL+"brwser--->"+browser);
+		String USERNAME = System.getProperty("username");
+		String PASSWORD= System.getProperty("password");
+		System.out.println("Test1-->class1");
+		System.out.println("url-->"+URL);
+		System.out.println("username-->"+USERNAME);
+		System.out.println("password-->"+PASSWORD);
+		//hi my name is siva
+	
+	}
+	@Test
+	public void parctice2() {
+		
+		System.out.println("Test1-->class1");
+		System.out.println("Good boy");
+		System.out.println("Good boy and  bad boy");
+
+		System.out.println("Good boy siva");
+		System.out.println("bad boy siva and arun");
+	
+	}
+	@Test
+	public void parctice3() {
+		System.out.println("Test1-->class1");
+		//hi good afternoon
+		System.out.println("hai shiva");
+		//hai
+	
+	}
+	
+	
+
+}
